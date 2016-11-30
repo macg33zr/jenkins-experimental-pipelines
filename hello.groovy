e247348406cbf54d30c3f141c5bdf90af9c974c1
macg33zr <bill.dennis@gmail.com>
2016-11-30 01:27:11 +0000

    Hello pipeline

---
+++ hello.groovy
@@ -0,0 +1,32 @@
+/**
+ *
+ * To use this:
+ *
+ * 0) Review my GitHub repos mentioned below and decide that you are happy to pull this onto your Jenkins server.
+ *
+ * 1) On a Folder in Jenkins or globally configure a shared pipeline library pointing to this GitHub repo:
+ *  https://github.com/macg33zr/jenkins-pipeline-experiments.git
+ *
+ * 2) Create a Jenkins Pipeline job pulling in the file hello.groovy from this repo:
+ *  https://github.com/macg33zr/jenkins-experimental-pipelines.git
+ *
+ */
+pipeline {
+
+    // All stages will run on any node
+    agent label: ""
+
+    stages {
+        stage('Hello') {
+            steps {
+                script {
+
+                    // Invokes the helloTest global var (https://github.com/macg33zr/jenkins-pipeline-experiments/blob/master/vars/helloTest.groovy)
+                    helloTest {
+                        message = "hello this is a test"
+                    }
+                }
+            }
+        }
+    }
+}

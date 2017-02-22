dc96ea7fab4da2617af44199f1a125797c881793
Bill Dennis <bill.dennis@gmail.com>
2017-02-22 21:31:46 +0000

    Experiments

--- hello-simple.groovy
+++ hello-simple.groovy
@@ -7,7 +7,7 @@ pipeline {
         stage('Hello') {
             steps {             
               echo "Hello"
-              sh 'set
+              sh 'set'
             }
         }
         stage('World') {
